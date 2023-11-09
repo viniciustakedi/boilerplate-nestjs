c7996f8dda82f430df02fe4aac8d3cc1b56fd4bc
viniciustakedi <viniciustakedi7@gmail.com>
2023-11-09 09:58:08 -0300

    Chore: Add modules on main app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule, UsersModule } from './api/module-exporter';
 
 @Module({
   imports: [
@@ -10,6 +11,8 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(String(process.env.MONGODB_URI)),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
